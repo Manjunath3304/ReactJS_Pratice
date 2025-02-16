57872303354becb5ff08b160ced984cee8727100
Manju <yaletimanjunath111@gmail.com>
2025-02-17 02:27:15 +0530

    routing commit

---
+++ thirdapp/src/App.js
@@ -0,0 +1,25 @@
+import {Link, Routes, Route} from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+
+function App() {
+  return (
+    <div>
+    <>
+    <button><Link to='/'>Home</Link></button>
+    <button><Link to='/about'>About</Link></button>
+    <button><Link to='/contact'>Contact</Link></button>
+    
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/about' element={<About />}></Route>
+      <Route path='/contact' element={<Contact />}></Route>
+
+    </Routes>
+    </>
+    </div>
+  );
+}
+
+export default App;
