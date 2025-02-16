db2e697f560c5649d7faa30ce6c2fc1838d5da48
Manju <yaletimanjunath111@gmail.com>
2025-02-16 17:03:43 +0530

    hca

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,7 +1,15 @@
-
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
 function App() {
   return(
-    <h1>Hello world</h1>
+    <div>
+      <h1>Hello world</h1>
+      <Home name={"manju"} age={25}/>
+      <Home name={"vijay"} age={23}/>
+      <About />
+      <Contact />
+    </div>
   );
 }
 
